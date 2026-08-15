788e6268e50b728e699337573526d7f7c00d92b3
Pedro Henrique <amaral.tassi@aluno.ifsp.edu.br>
2026-08-15 19:50:07 -0300

    Update start_screen.gd

--- scenes/main/start_screen.gd
+++ scenes/main/start_screen.gd
@@ -1,12 +1,12 @@
 extends Control
 
 
-#var main = preload("res://scenes/3D/control_room.tscn").instantiate()
-var main = preload("res://scenes/main/main.tscn").instantiate()
+
+var main = preload("res://scenes/3D/control_room.tscn")
 
 func _on_starter_button_pressed() -> void:
 	$ClickSound.play()
 	await get_tree().create_timer(0.3).timeout
 	$AnimationPlayer.play("FadeOut")
 	await $AnimationPlayer.animation_finished
-	get_tree().change_scene_to_node(main)
+	get_tree().change_scene_to_packed(main)
