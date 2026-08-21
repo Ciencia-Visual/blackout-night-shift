5ab0aea4372257eb18721d2c95b9a742e2e8a9da
Pedro Henrique <amaral.tassi@aluno.ifsp.edu.br>
2026-08-21 17:09:31 -0300

    Computador
    Os botões do computador exibem uma cor como placeholder das futuras interfaces. Botões estão maiores e "Sair está mais destacado. Ao clicar no botão, não trava mais o mouse em 1 pessoa e é possível continuar usando o painel.

--- scenes/3D/mainmenu.gd
+++ scenes/3D/mainmenu.gd
@@ -6,7 +6,27 @@ func _ready():
 func configurar_botoes():
 	for filho in get_children():
 		if filho is Button:
-			filho.pressed.connect(func(): _on_botao_clicado(filho))
+			filho.pressed.connect(_on_botao_clicado.bind(filho))
+
+func _alternar_interface(indice_alvo: int):
+	var botoes: Array[Button] = []
+	
+	for filho in get_children():
+		if filho is Button:
+			botoes.append(filho)
+			
+	for i in botoes.size():
+		var botao = botoes[i]
+		var deve_ficar_visivel = (i == indice_alvo)
+		
+		for subfilho in botao.get_children():
+			if subfilho is Control: 
+				subfilho.visible = deve_ficar_visivel
+
+func soltar_mouse():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	var pos_atual = get_viewport().get_mouse_position()
+	get_viewport().warp_mouse(pos_atual)
 
 func _on_botao_clicado(botao_pressionado: Button):
 	var nome = botao_pressionado.name
@@ -15,25 +35,31 @@ func _on_botao_clicado(botao_pressionado: Button):
 	print("Botão clicado: ", nome, " | Texto: ", texto)
 	
 	match nome:
+		"Energybutton":
+			abrir_energia()
 		"Camerabutton":
 			abrir_camera()
 		"Antenabutton":
 			abrir_antena()
-		"Energybutton":
-			abrir_energia()
 		"ExitButton":
 			desligar_pc()
 		_:
 			print("Ação padrão para o botão: ", texto)
 
+func abrir_energia():
+	soltar_mouse()
+	print("Executando: Abrir Painel de Energia")
+	_alternar_interface(0)
+
 func abrir_camera():
+	soltar_mouse()
 	print("Executando: Abrir Cameras")
+	_alternar_interface(1)
 
 func abrir_antena():
+	soltar_mouse()
 	print("Executando: Abrir Antena")
-
-func abrir_energia():
-	print("Executando: Abrir Painel de Energia")
+	_alternar_interface(2)
 
 func desligar_pc():
 	$"..".visible = false
