7f20623df3947ae73602fcd84c06e382c980242c
Pedro Henrique <amaral.tassi@aluno.ifsp.edu.br>
2026-08-21 22:42:20 -0300

    Update computer_ui.gd

--- scenes/panels/computer_ui.gd
+++ scenes/panels/computer_ui.gd
@@ -3,7 +3,6 @@ extends Control
 	
 func _ready():
 	hide()
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
